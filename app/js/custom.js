da10ea9e1e419f9067b6a3cbb459bf26c85fb33a
sebknight <37192621+sebknight@users.noreply.github.com>
2018-06-25 09:46:48 +1200

    css adjustments, beginning logic and plugins

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,46 +1,49 @@
 $(document).ready(function () {
-  $('#form').parsley();
-  $(function () {
-  var $sections = $('.form-section');
+  console.log(vehicles);
 
-  function navigateTo(index) {
-    // Mark the current section with the class 'current'
-    $sections
-      .removeClass('current')
-      .eq(index)
-        .addClass('current');
-    // Show only the navigation buttons that make sense for the current section:
-    $('.form-navigation .previous').toggle(index > 0);
-    var atTheEnd = index >= $sections.length - 1;
-    $('.form-navigation .next').toggle(!atTheEnd);
-    $('.form-navigation [type=submit]').toggle(atTheEnd);
-  }
+  // $('#form').parsley();
+  // $(function () {
 
-  function curIndex() {
-    // Return the current index by looking at which section has the class 'current'
-    return $sections.index($sections.filter('.current'));
-  }
-
-  // Previous button is easy, just go back
-  $('.form-navigation .previous').click(function() {
-    navigateTo(curIndex() - 1);
-  });
-
-  // Next button goes forward iff current block validates
-  $('.form-navigation .next').click(function() {
-    $('.demo-form').parsley().whenValidate({
-      group: 'block-' + curIndex()
-    }).done(function() {
-      navigateTo(curIndex() + 1);
-    });
-  });
-
-  // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
-  $sections.each(function(index, section) {
-    $(section).find(':input').attr('data-parsley-group', 'block-' + index);
-  });
-  navigateTo(0); // Start at the beginning
-});
+//   var $sections = $('.form-section');
+//
+//   function navigateTo(index) {
+//     // Mark the current section with the class 'current'
+//     $sections
+//       .removeClass('current')
+//       .eq(index)
+//         .addClass('current');
+//     // Show only the navigation buttons that make sense for the current section:
+//     $('.form-navigation .previous').toggle(index > 0);
+//     var atTheEnd = index >= $sections.length - 1;
+//     $('.form-navigation .next').toggle(!atTheEnd);
+//     $('.form-navigation [type=submit]').toggle(atTheEnd);
+//   }
+//
+//   function curIndex() {
+//     // Return the current index by looking at which section has the class 'current'
+//     return $sections.index($sections.filter('.current'));
+//   }
+//
+//   // Previous button is easy, just go back
+//   $('.form-navigation .previous').click(function() {
+//     navigateTo(curIndex() - 1);
+//   });
+//
+//   // Next button goes forward iff current block validates
+//   $('.form-navigation .next').click(function() {
+//     $('.demo-form').parsley().whenValidate({
+//       group: 'block-' + curIndex()
+//     }).done(function() {
+//       navigateTo(curIndex() + 1);
+//     });
+//   });
+//
+//   // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
+//   $sections.each(function(index, section) {
+//     $(section).find(':input').attr('data-parsley-group', 'block-' + index);
+//   });
+//   navigateTo(0); // Start at the beginning
+// });
 
 
 
@@ -58,21 +61,25 @@ $(document).ready(function () {
 // });
 //
 
+//UI transitions
 
-$( ".form__continue--step-one" ).click(function(){
+$( ".form__continue--step-one").click(function(){
   $(".intro").hide();
   $(".form__dates").show();
   $(".details").show();
-  $(".col-right").css('background-image', 'url("../images/group-bike.jpg")');
+  $(".col-right__intro").removeClass("col-right__intro");
+  $(".col-right").addClass("col-right__dates");
 });
 
-$( ".form__continue--step-two" ).click(function(){
+$( ".form__continue--step-two").click(function(){
   $(".form__dates").hide();
   $(".form__passengers").show();
   $(".form__continue--step-two").hide();
+  $(".col-right__dates").removeClass("col-right__dates");
+  $(".col-right").addClass("col-right__passengers");
 });
 
-$( ".form__continue--step-three" ).click(function(){
+$( ".form__continue--step-three").click(function(){
   $(".form__passengers").hide();
   $(".form__destination").show();
   $("#map").show();
@@ -90,5 +97,20 @@ $(".form__continue--step-five").click(function(){
   $(".form__vehicles").hide();
   $(".form__confirm").show();
 });
+
+
+
+// Aaaah section
+console.log(vehicles);
+
+var app = {
+  data: {},
+  arrStorage: [],
+  init: function (){
+
+  }
+};
+
+app.init();
 
 }()); //iife ENDS
