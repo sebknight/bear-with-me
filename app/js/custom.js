261371fb07fe1169cdf77c1794fe0230b6301832
sebknight <37192621+sebknight@users.noreply.github.com>
2018-06-27 13:26:04 +1200

    changing to mapbox js

--- app/js/custom.js
+++ app/js/custom.js
@@ -60,13 +60,13 @@ $( ".form__continue--step-one").click(function(){
 //   $("#map").show();
 // });
 
-$(".form__continue--step-four").click(function(){
-  $(".form__destination").hide();
-  $("#map").hide();
-  $(".form__vehicles").show();
-  $(".vehicle-options").addClass('is-visible');
-  $(".vehicle").addClass('is-disabled');
-})
+// $(".form__continue--step-four").click(function(){
+//   $(".form__destination").hide();
+//   $("#map").hide();
+//   $(".form__vehicles").show();
+//   $(".vehicle-options").addClass('is-visible');
+//   $(".vehicle").addClass('is-disabled');
+// })
 
 $(".form__continue--step-five").click(function(){
   $(".form__vehicles").hide();
@@ -78,17 +78,34 @@ $(".form__continue--step-five").click(function(){
 // check data.js is called
 console.log(vehicles);
 
-// DOM query for passenger and date details
+// DOM queries
 var detailsForm = document.getElementById('details-form');
+var distanceButton = document.getElementById('distance-button');
+
 //take passenger and date values and make them numbers
+// function makeNumber(){
+//   var datesString = document.getElementById('dates').value;
+//   console.log(datesString);
+//   var passengersString = document.getElementById('passengers').value;
+//   console.log(passengersString);
+//   // var datesNumber = function parseInt(datesString,[10]){};
+//   // var passengersNumber = function parseInt(passengersString,[10]){};
+//   console.log(typeof datesString,);
+//   console.log(typeof passengersString);
+//
+// };
+
 function makeNumber(){
   var datesString = document.getElementById('dates').value;
   console.log(datesString);
   var passengersString = document.getElementById('passengers').value;
   console.log(passengersString);
-  var datesNumber = function number(datesString){};
-  var passengersNumber = function number(passengersString){};
+  var datesNumber = parseInt(datesString);
+  var passengersNumber = parseInt(passengersString);
+  console.log(typeof datesNumber);
+  console.log(typeof passengersNumber);
 };
+
 
 var app = {
   data: {},
@@ -103,7 +120,28 @@ var app = {
               $("#map").show();
             makeNumber();
         });
-      }
+
+        distanceButton.addEventListener("submit", function(event){
+          event.preventDefault();
+          $(".form__destination").hide();
+          $("#map").hide();
+          $(".form__vehicles").show();
+          $(".vehicle-options").addClass('is-visible');
+          $(".vehicle").addClass('is-disabled');
+
+          //hide map, show vehicle choices
+          function getDistance(){
+            var distanceString = document.querySelector('div.mapbox-directions-route-summary > h1').value;
+            console.log(distanceString);
+            var distanceString = parseInt(distanceNumber);
+            console.log(distanceNumber);
+          }
+
+          getDistance();
+        })
+  // init ENDS
+  }
+  // var app ENDS
 };
 
 app.init();
