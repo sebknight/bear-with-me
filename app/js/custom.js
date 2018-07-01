dbe0e77553c4492d5052a256b8d005165c60c1f9
sebknight <37192621+sebknight@users.noreply.github.com>
2018-07-01 17:07:47 +1200

    css tweaks

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,6 @@
 //DOM queries
-var detailsForm = document.getElementById('details-form');
-var distanceButton = document.getElementById('distance-button');
+var detailsForm = document.getElementById("details-form");
+var distanceButton = document.getElementById("distance-button");
 var possibleVehicles = [];
 // var passengersNumber;
 // var datesNumber;
@@ -21,86 +21,95 @@ var dates = $("#from, #to").datepicker({
         dates.not(this).datepicker("option", option, date);
     };
   function calcDatesNumber() {
-   var date1 = $('#from').datepicker('getDate');
-   var date2 = $('#to').datepicker('getDate');
+   var date1 = $("#from").datepicker("getDate");
+   var date2 = $("#to").datepicker("getDate");
     datesNumber = 0;
    if (date1 && date2) {
      datesNumber = Math.floor((date2.getTime() - date1.getTime()) / 86400000);
    }
-   // $('#calculated').val(datesNumber);
  };
 
-$( ".form__continue--step-one").click(function(){
-  $(".intro").hide();
-  $(".form__dates").show();
-  $(".form__passengers").show();
-  $(".details").show();
-  $(".col-right__intro").removeClass("col-right__intro");
-  $(".col-right").addClass("col-right__dates");
-});
+  $( ".form__continue--step-one").click(function(){
+    $(".intro").hide();
+    $(".form__dates").show();
+    $(".form__passengers").show();
+    $(".details").show();
+    $(".col-right__intro").removeClass("col-right__intro");
+    $(".col-right").addClass("col-right__dates");
+  });
+
+  $(".form__continue--step-five").click(function(){
+    $(".form__vehicles").hide();
+    $(".vehicle-options").hide();
+    $(".form__destination").show();
+    $("#map").show();
+  });
+
+  // Aaaah section
+  // check data.js is called
+  console.log(vehicles);
+
+  // function showPossibleVehicles(x,y){
+  //   ($).css("x","y");
+  // }
+  //
+  // UI transition to show mapbox, called if user data validates
+  function moveToVehicles(){
+    $(".form__dates").hide();
+    $(".form__passengers").hide();
+    $(".form__vehicles").show();
+    $(".vehicle-options").css("visibility","visible");
+    // map.invalidateSize();
+  };
+
+  // Take dates and passenger inputs and make them numbers
+  function getPassengerNumber(){
+    // datesString = document.getElementById("#calculated").value;
+    // console.log(datesString);
+    passengersString = document.getElementById("passengers").value;
+    console.log(passengersString);
+    // datesNumber = parseInt(datesString);
+    passengersNumber = parseInt(passengersString);
+    // console.log(typeof datesNumber);
+    console.log(typeof passengersNumber);
+  };
 
 
-$(".form__continue--step-five").click(function(){
-  $(".form__vehicles").hide();
-  $(".form__confirm").show();
-});
-
-
-// Aaaah section
-// check data.js is called
-console.log(vehicles);
-
-// UI transition to show mapbox, called if user data validates
-function moveToMap(){
-  $(".form__dates").hide();
-  $(".form__passengers").hide();
-  $(".form__destination").show();
-  $("#map").show();
-  map.invalidateSize();
-};
-
-
-// Take dates and passenger inputs and make them numbers
-function getPassengerNumber(){
-  // datesString = document.getElementById('#calculated').value;
-  // console.log(datesString);
-  passengersString = document.getElementById('passengers').value;
-  console.log(passengersString);
-  // datesNumber = parseInt(datesString);
-  passengersNumber = parseInt(passengersString);
-  // console.log(typeof datesNumber);
-  console.log(typeof passengersNumber);
-};
-
-
-// pushing available vehicles to array based on passenger number and days
-function findPossibleVehicles() {
-  if (passengersNumber === 1 && datesNumber < 3) {
-    possibleVehicles.push(vehicles.motorbike, vehicles.smallCar);
-    console.log(possibleVehicles);
-    moveToMap();
+  // pushing available vehicles to array based on passenger number and days
+  function findPossibleVehicles() {
+    if (passengersNumber === 1 && datesNumber < 3){
+      possibleVehicles.push(vehicles.motorbike, vehicles.smallCar);
+      console.log(possibleVehicles);
+      $(".motorbike", ".small-car").show();
+      moveToVehicles();
+    }
+    else if (passengersNumber < 3 && datesNumber < 11){
+      possibleVehicles.push(vehicles.smallCar,vehicles.bigCar);
+      console.log(possibleVehicles);
+      moveToVehicles();
+      $(".small-car",".big-car").show();
+    }
+    else if (passengersNumber < 6 && datesNumber > 2 && datesNumber < 11){
+      possibleVehicles.push(vehicles.bigCar, vehicles.camper);
+      console.log(possibleVehicles);
+      moveToVehicles();
+      $(".big-car",".camper").show();
+    }
+    else if (passengersNumber > 1 && datesNumber > 10){
+      possibleVehicles.push(vehicles.camper);
+      console.log(possibleVehicles);
+      moveToVehicles();
+      $(".camper").show();
+    }
+    else {
+      console.log(possibleVehicles);
+      console.log("no vehicles available");
+    }
   }
-  else if (passengersNumber < 3 && datesNumber < 11){
-    possibleVehicles.push(vehicles.smallCar,vehicles.bigCar);
-    console.log(possibleVehicles);
-    moveToMap();
-  }
-  else if (passengersNumber < 6 && datesNumber > 2 && datesNumber <11){
-    possibleVehicles.push(vehicles.bigCar, vehicles.camper);
-    console.log(possibleVehicles);
-    moveToMap();
-  }
-  else if (passengersNumber > 1 && datesNumber > 10){
-    possibleVehicles.push(vehicles.camper);
-    console.log(possibleVehicles);
-    moveToMap();
-  }
-  else {
-    console.log(possibleVehicles);
-    console.log('no vehicles available');
-    $();
-  }
-}
+
+// function showPossibleVehicles(){
+//   possibleVehicles.
+// }
 
 var app = {
   data: {},
@@ -116,6 +125,7 @@ var app = {
             // determine available vehicles based on passenger numbers and dates
             findPossibleVehicles();
         // details submit button function ENDS
+            moveToVehicles();
         });
 
         // adds event listener to take map data
@@ -123,9 +133,8 @@ var app = {
           event.preventDefault();
           $(".form__destination").hide();
           $("#map").hide();
-          $(".form__vehicles").show();
-          $(".vehicle-options").addClass('is-visible');
-          $(".vehicle").addClass('is-disabled');
+          // $(".vehicle-options").addClass("is-visible");
+          // $(".vehicle").addClass("is-disabled");
         // destination input function ENDS
         });
 
